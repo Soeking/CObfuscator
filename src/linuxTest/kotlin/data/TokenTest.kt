c03a400e2a76039f08642e08913cee38d1b2ba09
Soeking <great.from.g@gmail.com>
2019-11-15 19:02:57 +0900

    fix test

--- src/linuxTest/kotlin/data/TokenTest.kt
+++ src/linuxTest/kotlin/data/TokenTest.kt
@@ -1,12 +1,14 @@
 package data
 
 import kotlin.test.Test
-import kotlin.test.assertTrue
+import kotlin.test.assertEquals
 
 class TokenTest {
     @Test
     fun tokenTest() {
-        assertTrue(Token("abc").type == TokenType.VARIABLE)
-        assertTrue(Token("2.9").type == TokenType.NUMBER)
+        assertEquals(Token("abc").type, TokenType.VARIABLE)
+        assertEquals(Token("2.9").type, TokenType.NUMBER)
+        assertEquals(Token("while").type, TokenType.CONTROL)
+        assertEquals(Token(";").type, TokenType.BRACKET)
     }
 }
